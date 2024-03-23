a82bd1b46339e6b14f91b4d4e9b7e32d87fed366
yuval1337 <68990745+yuval1337@users.noreply.github.com>
2024-03-23 19:45:40 +0200

    Update

--- matfast/Matrix.py
+++ matfast/Matrix.py
@@ -2,19 +2,14 @@ from typing import List
 
 class Matrix:
     def __init__(self, matrix_like: List[List[float]]):
-        self._M  = len(matrix_like)
-        self._N = len(matrix_like[0])
-        if any([len(row) != self._N for row in matrix_like[1:]]):
+        self.shape  = (len(matrix_like), len(matrix_like[0])) # M, N
+        if any([len(row) != self.shape[1] for row in matrix_like[1:]]):
             raise ValueError
         self._value = matrix_like
 
-    @property    
-    def shape(self):
-        return (self._M, self._N)
-
     def __mul__(self, other: "Matrix") -> "Matrix":
-        if self.shape != other.shape:
-            raise ValueError
+        if self.shape[0] != other.shape[1]:
+            raise ValueError(f"invalid shape: {self.shape[0]}, {other.shape[1]} ?")
         return Matrix([[
                     sum([self._value[row][idx] * other._value[idx][col] for idx in range(self.shape[1])])
                 for col in range(other.shape[1])]
@@ -23,15 +18,23 @@ class Matrix:
 
     def __repr__(self) -> str:
         result = "Matrix(["
-        for idx, row in enumerate(self._value):
-            result += str(row)
-            if idx < self.shape[0] - 1:
-                result += ","
+        if self.shape[0] <= 3:
+            for idx, row in enumerate(self._value[0:]):
+                result += str(row)
+                if idx < self.shape[0] - 1:
+                    result += ","
+        else:
+            for idx, row in enumerate(self._value[:2]+self._value[-1:]):
+                if idx < 2:
+                    result += str(row) + ","
+                else:
+                    result += "..." + str(row)
+
         result += "])"
         return result
 
 
 if __name__ == "__main__":
-    m1 = Matrix([[1,2,3],[4,5,6],[7,8,9]])
-    m2 = Matrix([[0,0,1],[0,1,0],[1,0,0]]) # Permutation matrix
-    print(m1 * m2)
+    m1 = Matrix([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
+    m2 = Matrix([[0,0,0,1],[0,0,1,0],[0,1,0,0],[1,0,0,0]]) # Permutation matrix
+    print(m1 * m1)
